ffc858915b811d2d70080811fe49d81749b9fdef
Vearsmon <vearsmon285537@gmail.com>
2024-05-29 04:58:12 +0500

    Readme and minor changes

---
+++ Core/Migrations/20240528235517_startMigration.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class startMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    user_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_user_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    email_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: true),
+                    security_stamp = table.Column<string>(type: "text", nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true),
+                    phone_number = table.Column<string>(type: "text", nullable: true),
+                    phone_number_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    two_factor_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    lockout_end = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    lockout_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    access_failed_count = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "head_entity",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    fullname = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_head_entity", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "institute_entity",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_institute_entity", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "module_entity",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_module_entity", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "program_entity",
+                columns: table => new
+                {
+                    uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    cypher = table.Column<string>(type: "text", nullable: false),
+                    level = table.Column<string>(type: "text", nullable: false),
+                    standard = table.Column<string>(type: "text", nullable: false),
+                    institute = table.Column<Guid>(type: "uuid", nullable: false),
+                    head = table.Column<Guid>(type: "uuid", nullable: false),
+                    accreditation_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_program_entity", x => x.uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "program_module_mapping_entity",
+                columns: table => new
+                {
+                    program_uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    module_uuid = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_program_module_mapping_entity", x => new { x.program_uuid, x.module_uuid });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    role_id = table.Column<string>(type: "text", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_role_claims", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_asp_net_role_claims_asp_net_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_claims", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_claims_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    login_provider = table.Column<string>(type: "text", nullable: false),
+                    provider_key = table.Column<string>(type: "text", nullable: false),
+                    provider_display_name = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_logins", x => new { x.login_provider, x.provider_key });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_logins_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    role_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_roles", x => new { x.user_id, x.role_id });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_roles_asp_net_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_roles_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    login_provider = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_tokens", x => new { x.user_id, x.login_provider, x.name });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_tokens_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "id", "concurrency_stamp", "name", "normalized_name" },
+                values: new object[] { "4e5c0dd9-d041-4a52-a449-fa69f6184bbe", null, "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "id", "access_failed_count", "concurrency_stamp", "email", "email_confirmed", "lockout_enabled", "lockout_end", "normalized_email", "normalized_user_name", "password_hash", "phone_number", "phone_number_confirmed", "security_stamp", "two_factor_enabled", "user_name" },
+                values: new object[] { "2f775ba4-4f49-414c-8d7c-a78409f3639d", 0, "47e6bbfa-f086-4a96-9e7d-2d1f32176434", "admin@email.com", true, false, null, "ADMIN@EMAIL.COM", "ADMIN", "AQAAAAIAAYagAAAAELiCZyOnPDlJcdMcMjCYHXHay8ABkQJUiXnIVKj1y6hU8o3IFW8G8WVMDJvpGB+/mA==", null, false, "", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "head_entity",
+                columns: new[] { "uuid", "fullname" },
+                values: new object[,]
+                {
+                    { new Guid("0e887f76-2a05-11e1-b174-00259030b74f"), "Обухов Олег Владимирович" },
+                    { new Guid("62850c3d-e440-4969-9456-8dfe9c329f96"), "Кузьмин Федор Петрович" },
+                    { new Guid("8ff4c2c6-9d6b-44fd-93bc-550dd2f154a2"), "Овчинников Максим Михайлович" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "institute_entity",
+                columns: new[] { "uuid", "title" },
+                values: new object[,]
+                {
+                    { new Guid("41d6b9cb-bfc0-4619-9d01-9125f407d458"), "Институт новых материалов и технологий - ИНМТ" },
+                    { new Guid("6a0099be-2bd4-409b-aee2-10984c4df380"), "Институт радиоэлектроники и информационных технологий - РтФ" },
+                    { new Guid("a71450fb-ad84-4d75-b226-25ce4a2f1648"), "Институт естественных наук и математики - ИЕНиМ" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "module_entity",
+                columns: new[] { "uuid", "title", "type" },
+                values: new object[,]
+                {
+                    { new Guid("54e561d2-0a27-4644-b67a-47ab4e7881bc"), "Растровая графика", "STANDARD" },
+                    { new Guid("a50eefaa-d67c-4917-a143-c194ec1b9929"), "Основы астрономии", "PROJECT" },
+                    { new Guid("ad25a898-4364-4a1a-b9df-b10d68c3a093"), "Векторная графика", "STANDARD" },
+                    { new Guid("b2b666ed-728b-4bd1-8840-72eeda5a3a31"), "Психология предпринимательства", "MINOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "program_entity",
+                columns: new[] { "uuid", "accreditation_time", "cypher", "head", "institute", "level", "standard", "status", "title" },
+                values: new object[] { new Guid("30a12709-e029-400d-85a4-582109fbb923"), new DateTime(2025, 3, 13, 19, 0, 0, 0, DateTimeKind.Utc), "09.04.03/33.03", new Guid("0e887f76-2a05-11e1-b174-00259030b74f"), new Guid("6a0099be-2bd4-409b-aee2-10984c4df380"), "Магистратура", "СУОС", "Действующая до завершения срока освоения", "Разработка программно-информационных систем" });
+
+            migrationBuilder.InsertData(
+                table: "program_module_mapping_entity",
+                columns: new[] { "module_uuid", "program_uuid" },
+                values: new object[,]
+                {
+                    { new Guid("54e561d2-0a27-4644-b67a-47ab4e7881bc"), new Guid("30a12709-e029-400d-85a4-582109fbb923") },
+                    { new Guid("ad25a898-4364-4a1a-b9df-b10d68c3a093"), new Guid("30a12709-e029-400d-85a4-582109fbb923") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "role_id", "user_id" },
+                values: new object[] { "4e5c0dd9-d041-4a52-a449-fa69f6184bbe", "2f775ba4-4f49-414c-8d7c-a78409f3639d" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_role_claims_role_id",
+                table: "AspNetRoleClaims",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "normalized_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_claims_user_id",
+                table: "AspNetUserClaims",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_logins_user_id",
+                table: "AspNetUserLogins",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_roles_role_id",
+                table: "AspNetUserRoles",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "normalized_email");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "normalized_user_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "head_entity");
+
+            migrationBuilder.DropTable(
+                name: "institute_entity");
+
+            migrationBuilder.DropTable(
+                name: "module_entity");
+
+            migrationBuilder.DropTable(
+                name: "program_entity");
+
+            migrationBuilder.DropTable(
+                name: "program_module_mapping_entity");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
